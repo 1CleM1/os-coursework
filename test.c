3bfe5374580fdb6da94d34662b84b5c43add89e2
Clemens Beese <clemenbe@stud.ntnu.no>
2026-08-29 21:26:19 +0200

    first c commit

---
+++ test.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+	int x = 42;
+	printf("Hello, compiler works!\n");
+	printf("test value: %d\n", x);
+	printf("Pointer size: %zu bytes\n", sizeof(void*);
+
+	return 0;
+}
+
+
